deb1b1528e183c07b2fed0ea9bab3a443bc02bae
Zervyrel <85784958+Zervyrel@users.noreply.github.com>
2022-03-24 21:27:24 +0100

    Create Archion.py

---
+++ Archion.py
@@ -0,0 +1,23 @@
+import os
+import json
+import discord
+from discord.ext import commands, tasks
+
+client = commands.Bot(
+    commands.when_mentioned_or(*"."),
+    intents=discord.Intents.all(),
+    help_command=None,
+    case_intensitive=True,
+    debug_guilds=[id])
+
+if __name__ == "__main__":
+    for filename in os.listdir("Cogs"):
+        if filename.endswith(".py"):
+            client.load_extension(f"Cogs.{filename[:-3]}")
+client.load_extension("jishaku")
+
+with open("Database/Config.json", "r") as file:
+    data = json.load(file)
+    token = data["token"]
+
+client.run(token)
