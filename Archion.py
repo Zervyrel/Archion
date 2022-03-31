8a3013616a740bac86f9abc04d54e81bb8340d54
Zervyrel <85784958+Zervyrel@users.noreply.github.com>
2022-03-31 21:51:43 +0200

    Update Archion.py

--- Archion.py
+++ Archion.py
@@ -9,13 +9,25 @@ client = commands.Bot(
     commands.when_mentioned_or(*"^"),
     intents=discord.Intents.all(),
     help_command=None,
-    case_intensitive=True)
+    case_intensitive=True,
+    debug_guilds=[903607882564636683])
 
 if __name__ == "__main__":
     for filename in os.listdir("Cogs"):
         if filename.endswith(".py"):
             client.load_extension(f"Cogs.{filename[:-3]}")
 client.load_extension("jishaku")
+
+with open("Database/Config.json", "r") as file:
+    data = json.load(file)
+    token = data["token"]
+
+@client.slash_command(description="Invites the Archion bot to your Server")
+async def invite(ctx):
+    button = Button(label="Invite", url=f"https://discord.com/api/oauth2/authorize?client_id={client.user.id}&permissions=2147601408&scope=bot%20applications.commands")
+    view = View()
+    view.add_item(button)
+    await ctx.respond("To invite me press the Button below this message", view=view)
 
 @client.command()
 async def shutdown(ctx):
@@ -27,9 +39,5 @@ async def restart(ctx):
     await ctx.send("Restarting...")
     os.system("clear")
     os.execv(sys.executable, ["python"] + sys.argv)
-
-with open("Database/Config.json", "r") as file:
-    data = json.load(file)
-    token = data["token"]
 
 client.run(token)
