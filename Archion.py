35a7577076fc0a47ebd12bd25de71dca71d52a72
Zervyrel <85784958+Zervyrel@users.noreply.github.com>
2022-04-11 03:11:56 +0200

    Update Archion.py

--- Archion.py
+++ Archion.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import discord
+from rich.console import Console
 from discord.ext import commands, bridge
 
 client = bridge.Bot(
@@ -11,10 +12,13 @@ client = bridge.Bot(
     case_intensitive=True,
     debug_guilds=[guildID]) #Remove this line to get global slash commands
                             #debug_guilds is for testing the slash commands on the test Server
+console = Console()
+
 if __name__ == "__main__":
     for filename in os.listdir("Cogs"):
         if filename.endswith(".py"):
             client.load_extension(f"Cogs.{filename[:-3]}")
+            console.print(filename, style="bold underline purple")
 client.load_extension("jishaku")
 
 with open("Database/Config.json", "r") as file:
